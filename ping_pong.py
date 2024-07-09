884d0c0cfd9d154eb3bc9943f2e9e966ff59a959
TheoTsio <teo.tsiolakis@gmail.com>
2024-07-09 16:07:01 +0300

    'RacketClassCreation'

--- ping_pong.py
+++ ping_pong.py
@@ -4,8 +4,40 @@ from pygame import *
 window = display.set_mode((700, 500))
 window.fill((200, 255, 255))
 
+class GameSprite(sprite.Sprite):
+    #class constructor
+    def __init__(self, player_image, player_x, player_y, player_speed, size_x, size_y):
+        super().__init__()
+        # each sprite must store an image property
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        # each sprite must store the rect property it is inscribed in
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
 
 
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Racket(GameSprite):
+    def move_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP]:
+            self.rect.y -= self.speed
+        if keys[K_DOWN]:
+            self.rect.y += self.speed
+    def move_l(self):
+        keys = key.get_pressed()
+        if keys[K_w]:
+            self.rect.y -= self.speed
+        if keys[K_s]:
+            self.rect.y += self.speed
+
+
+racket_l = Racket('racket.png', 20, 100, 10, 20, 110)
+racket_r = Racket('racket.png', 680, 100, 10, 20, 110)
 
 game = True
 clock = time.Clock()
@@ -14,6 +46,13 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    window.fill((200, 255, 255))
+    racket_l.reset()
+    racket_l.move_l()
+
+
+    racket_r.reset()
+    racket_r.move_r()
 
     display.update()
     clock.tick(FPS)
